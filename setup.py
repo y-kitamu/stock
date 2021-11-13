891235c93956f4de9d228495800dae11a0ad6160
Kitamura <ymyk6602@gmail.com>
2021-11-13 15:37:44 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+init = os.path.join(os.path.dirname(__file__), "src", "{0}", "__init__.py")
+version_line = list(filter(lambda l: l.startswith("__version__"), open(init)))[0]
+VERSION = ".".join(["{{}}".format(x) for x in eval(version_line.split("=")[-1])])
+
+
+def load_requirements(f):
+    return list(
+        filter(None,
+               [l.split("#", 1)[0].strip() for l in open(os.path.join(os.getcwd(), f)).readlines()]))
+
+
+setup(
+    author="Yusuke Kitamura",
+    author_email='ymyk6602@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Analyze stock market",
+    install_requires=load_requirements("requirements.txt"),
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='stock',
+    name='stock',
+    packages=find_packages(include=['stock', 'stock.*']),
+    test_suite='tests',
+    tests_require=load_requirements("requirements_dev.txt"),
+    url='https://github.com/y-kitamu/stock',
+    version=VERSION,
+    zip_safe=False,
+)
---
+++ stock/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for stock."""
+
+__author__ = "Yusuke Kitamura"
+__email__ = 'ymyk6602@gmail.com'
+__version__ = (0, 1, 0)
