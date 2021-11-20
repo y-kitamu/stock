74062ed9321b2438f16c3b8c2f3e038c389e3cab
Kitamura <ymyk6602@gmail.com>
2021-11-20 14:13:11 +0900

    bugfix

--- cert/decrypt.sh
+++ cert/decrypt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+CERT_DIR=$(dirname ${BASH_SOURCE[0]})
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
-    --output stockdata-332410-704571e36294.json stockdata-332410-704571e36294.json.gpg
+    --output ${CERT_DIR}/stockdata-332410-704571e36294.json ${CERT_DIR}/stockdata-332410-704571e36294.json.gpg
