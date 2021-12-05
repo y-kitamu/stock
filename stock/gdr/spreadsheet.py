82f3f5e1fee289d19e85e3da38b4ca334f31ed92
Kitamura <ymyk6602@gmail.com>
2021-12-05 14:13:15 +0900

    wip

--- stock/gdr/spreadsheet.py
+++ stock/gdr/spreadsheet.py
@@ -4,3 +4,10 @@ Author : Yusuke Kitamura
 Create Date : 2021-12-05 11:17:42
 Copyright (c) 2019- Yusuke Kitamura <ymyk6602@gmail.com>
 """
+from typing import Any, List
+
+UPLOAD_LOG_SS_GID = "1SRi3cDXctknMSMB1TrEctq4vVeqC4C3ZLEkmY-A1CII"
+
+
+def add_row_to_ss(row: List[Any], sid: str = UPLOAD_LOG_SS_GID):
+    pass
