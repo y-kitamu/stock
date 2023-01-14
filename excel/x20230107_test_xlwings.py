effe67365ed0e44c9806315c87c75ce676143f41
kitamura <ymyk6602@gmail.com>
2023-01-14 22:11:21 +0900

    test xlwings

--- excel/x20230107_test_xlwings.py
+++ excel/x20230107_test_xlwings.py
@@ -1,5 +1,8 @@
+import time
+
 import numpy as np
 import xlwings as xw
+
 
 def main():
     wb = xw.Book.caller()
@@ -7,6 +10,17 @@ def main():
 
 
 @xw.func
+def my_macro():
+    """Writes the name of the Workbook into Range("A1") of Sheet 1"""
+    wb = xw.Book.caller()
+    print(wb.sheets[0].name, wb.name, wb.sheets[0].range('A1').value)
+    wb.sheets[0].range('A5').value = wb.name
+    return wb.name
+
+@xw.func
 def double_sum(x, y):
     """Returns twice the sum of the two arguments"""
-    return 2 * (x + y)    
+    return 2 * (x + y)
+
+if __name__ == "__main__":
+    xw.serve()
