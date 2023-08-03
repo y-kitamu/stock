51b1e35884c3fea0cb542c238db7ff452750dae0
Kitamura <ymyk6602@gmail.com>
2023-08-03 10:34:54 +0900

    add financial data

---
+++ scripts/old/collect_financial_infos.py
@@ -0,0 +1,237 @@
+"""collect_financial_infos.py
+
+Author : Yusuke Kitamura
+Create Date : 2023-07-30 18:27:33
+Copyright (c) 2019- Yusuke Kitamura <ymyk6602@gmail.com>
+"""
+import argparse
+import csv
+import datetime
+import json
+import time
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import stock
+
+
+def convert_to_number(str):
+    """Convert string to number."""
+    str = str.replace(",", "")
+
+    factor = 1
+    if str[-1] == "k":
+        factor = 1000
+        str = str[:-1]
+
+    if "." in str:
+        return float(str) * factor
+    else:
+        try:
+            return int(str) * factor
+        except:
+            # print("Failed to convert {} to number".format(str))
+            return np.nan
+
+
+def extract_table_data(source: BeautifulSoup) -> dict:
+    """Extract table data (finance data) from BeautifulSoup object."""
+    table = {}
+    table_head = source.find("div", {"class": "D(tbhg)"})
+    # if table_head is None:
+    #     stock.logger.warning("Failed to extract table data")
+    #     return table
+    columns = [span.text for span in table_head.find_all("span")]
+
+    table_body = source.find("div", {"class": "D(tbrg)"})
+    # if table_body is None:
+    #     stock.logger.warning("Failed to extract table data")
+    #     return table
+    blocks = [div for div in table_body.find_all("div", {"class": "rw-expnded"}, recursive=False)]
+
+    for block in blocks:
+        #
+        rows = block.find_all("div", {"class": "D(tbr)"})
+        first_row = rows[0].find("div", {"class": "D(tbc)"})
+        # if first_row is None:
+        #     stock.logger.warning("Failed to extract row data")
+        #     continue
+        breakdown = first_row.text
+        table[breakdown] = {
+            key: convert_to_number(val.text)
+            for key, val in zip(columns[1:], rows[0].find_all("div", {"class": "Ta(c)"}))
+            if key != "ttm"
+        }
+        # 折りたたまれている行の読み込み
+        for row in rows[1:]:
+            sub_row = row.find("div", {"class": "D(tbc)"})
+            # if sub_row is None:
+            #     stock.logger.warning("Failed to extract sub-row data")
+            #     continue
+            sub_breakdown = sub_row.text
+            table[breakdown][sub_breakdown] = {
+                key: convert_to_number(val.text)
+                for key, val in zip(columns[1:], row.find_all("div", {"class": "Ta(c)"}))
+                if key != "ttm"
+            }
+    return table
+
+
+def run(
+    driver: Optional[webdriver.Remote] = None,
+    code: str = "NVDA",
+    output_dir: Path = stock.DATA_DIR / "financials",
+):
+    stock.logger.info(f"Start collecting financial data of {code}")
+
+    target_urls = [
+        "https://finance.yahoo.com/quote/{code}/financials?p={code}",
+        "https://finance.yahoo.com/quote/{code}/balance-sheet?p={code}",
+        "https://finance.yahoo.com/quote/{code}/cash-flow?p={code}",
+    ]
+
+    table = {"Quarterly": {}, "Annual": {}}
+    for url in target_urls:
+        driver.get(url.format(code=code))
+        buttons = [
+            button
+            for button in driver.find_elements(By.TAG_NAME, "button")
+            if button.text == "Expand All"
+        ]
+        if buttons:
+            buttons[0].click()
+            time.sleep(0.2)
+
+        # quqrterly data
+        buttons = [
+            button
+            for button in driver.find_elements(By.TAG_NAME, "button")
+            if button.text == "Quarterly"
+        ]
+        if buttons:
+            buttons[0].click()
+            time.sleep(0.2)
+        source = BeautifulSoup(driver.page_source, parser="lxml", features="lxml")
+        table["Quarterly"].update(extract_table_data(source))
+
+        # annual data
+        buttons = [
+            button
+            for button in driver.find_elements(By.TAG_NAME, "button")
+            if button.text == "Annual"
+        ]
+        if buttons:
+            buttons[0].click()
+            time.sleep(0.2)
+        source = BeautifulSoup(driver.page_source, parser="lxml", features="lxml")
+        table["Annual"].update(extract_table_data(source))
+
+    # save data
+    output_dir.mkdir(exist_ok=True)
+    json_path = output_dir / f"{code}.json"
+
+    # if json file exists, update it
+    if json_path.exists():
+        with open(json_path, "r") as f:
+            table.update(json.load(f))
+
+    with open(json_path, "w") as f:
+        json.dump(table, f, indent=4)
+
+    stock.logger.info(f"Saved financial data of {code} to {json_path}")
+
+
+# def run_all(codes: list[str] = [], output_dir: Path = stock.DATA_DIR / "financials") -> tuple[list[str], list[str]]:
+#     # prepare selenium driver
+#     stock.logger.info("Start ChromeDriver")
+#     options = webdriver.ChromeOptions()
+#     options.add_experimental_option("detach", True)
+#     options.add_argument("--headless")
+#     driver = webdriver.Chrome(options=options)
+#     stock.logger.info("ChromeDriver is ready")
+
+#     success_list = []
+#     failed_list = []
+
+#     try:
+#         for code in codes:
+#             if (stock.DATA_DIR / "financials" / f"{code}.json").exists():
+#                 stock.logger.info(f"Skip {code}")
+#                 continue
+#             try:
+#                 run(driver, code, output_dir)
+#                 success_list.append(code)
+#             except Exception as e:
+#                 stock.logger.warning(f"Failed to collect financial data of {code}: {e}")
+#                 failed_list.append(code)
+#     finally:
+#         driver.quit()
+
+#     return success_list, failed_list
+
+
+def main(debug: bool = False):
+    stock.logger.info("Start collecting financial data")
+
+    success_list = []
+    failed_list = []
+
+    # load stock code list
+    with open(stock.DATA_DIR / "us_stock_codes.csv", "r") as f:
+        csv_reader = csv.reader(f)
+        target_codes = [row[0] for row in list(csv_reader)[1:]]
+
+    # prepare selenium driver
+    stock.logger.info("Start ChromeDriver")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    if debug:
+        driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
+    else:
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+    stock.logger.info("ChromeDriver is ready")
+
+    try:
+        for code in target_codes:
+            if (stock.DATA_DIR / "financials" / f"{code}.json").exists():
+                stock.logger.info(f"Financial data of {code} already exists")
+                continue
+            # tasks[code] = executor.submit(run, code=code)
+            try:
+                run(driver=driver, code=code)
+                success_list.append(code)
+            except:
+                stock.logger.exception(f"Failed to collect financial data of {code}")
+                failed_list.append(code)
+    except:
+        stock.logger.exception("Unexpected error occurred")
+    finally:
+        driver.quit()
+
+    # log results
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    log_file = stock.LOG_DIR / "success_list_{}.txt".format(timestamp)
+    with open(log_file, "w") as f:
+        f.write("\n".join(success_list))
+
+    log_file = stock.LOG_DIR / "failed_list_{}.txt".format(timestamp)
+    with open(log_file, "w") as f:
+        f.write("\n".join(failed_list))
+
+    stock.logger.info("Finished collecting financial data")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true")
+
+    args = parser.parse_args()
+
+    main(args.debug)
---
+++ scripts/update_financial_data.py
@@ -0,0 +1,103 @@
+"""update_financial_data.py
+
+Update financial data from Yahoo Finance.
+Data is saved in ${PROJECT_ROOT}/data/codes/${ticker}.json.
+33
+Author : Yusuke Kitamura
+Create Date : 2023-08-03 09:50:02
+Copyright (c) 2019- Yusuke Kitamura <ymyk6602@gmail.com>
+"""
+import argparse
+import csv
+import json
+from pathlib import Path
+
+import requests
+import yfinance as yf
+from pyrate_limiter import Duration, Limiter, RequestRate
+from requests import Session
+from requests_cache import CacheMixin, SQLiteCache
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
+
+import stock
+
+TICKER_LIST_URL = "https://www.trkd-asia.com/rakutensec/exportcsvus?all=on&vall=on&forwarding=na&target=0&theme=na&returns=na&head_office=na&name=&code=&sector=na&pageNo=&c=us&p=result&r1=on"
+
+
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    pass
+
+
+session = CachedLimiterSession(
+    limiter=Limiter(RequestRate(2, Duration.SECOND * 0.2)),
+    bucket_class=MemoryQueueBucket,
+    backend=SQLiteCache("yfinance.cache"),
+)
+
+
+def update_ticker_list(ticker_list_path: Path, ticker_list_url: str = TICKER_LIST_URL):
+    try:
+        res = requests.get(ticker_list_url)
+        if res.status_code == 200:
+            with open(ticker_list_path, "w") as f:
+                f.write(res.text)
+        else:
+            stock.logger.error(
+                f"Failed to get ticker list. Status code: {res.status_code}\n{res.text}"
+            )
+    except requests.exceptions.ConnectionError:
+        raise ConnectionError("Connection Error. Check your network connection.")
+    except requests.exceptions.Timeout:
+        raise TimeoutError("Timeout Error. Check your network connection.")
+    except:
+        raise Exception("Unknown Error.")
+
+
+def update_financial_data(ticker: str, output_dir: Path):
+    """Update financial data (fundamentals) from Yahoo Finance.
+    If the output file already exists, update it.
+    """
+    output_path = output_dir / f"{ticker}.json"
+    data = {}
+    if output_path.exists():
+        try:
+            with open(output_path, "r") as f:
+                data = json.load(f)
+        except:
+            stock.logger.error(f"Failed to load {output_path}.")
+
+    new_data = yf.Ticker(ticker, session=session).quarterly_financials
+    for key, value in new_data.to_dict().items():
+        data[key.strftime("%Y-%m-%d")] = value
+
+    with open(output_path, "w") as f:
+        json.dump(data, f, indent=4)
+    stock.logger.info(f"Saved {output_path}.")
+
+
+def main(ticker_list_path: Path, output_dir: Path = stock.DATA_DIR / "codes"):
+    """ """
+    update_ticker_list(ticker_list_path=ticker_list_path)
+
+    with open(ticker_list_path, "r") as f:
+        reader = csv.reader(f)
+        next(reader)
+        tickers = [row[0] for row in reader]
+
+    for ticker in tickers:
+        try:
+            update_financial_data(ticker=ticker, output_dir=output_dir)
+        except KeyboardInterrupt:
+            stock.logger.exception("Keyboard Interrupt.")
+            break
+        except:
+            stock.logger.exception(f"Failed to update financial data. : {ticker}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ticker_list", type=Path, default=stock.DATA_DIR / "us_stock_codes.csv")
+    parser.add_argument("--output_dir", type=Path, default=stock.DATA_DIR / "codes")
+    args = parser.parse_args()
+
+    main(ticker_list_path=args.ticker_list, output_dir=args.output_dir)
