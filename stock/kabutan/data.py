b7b176fdfdee118af3e7c44dade58ead204ccb20
kitamura <ymyk6602@gmail.com>
2024-04-07 18:53:29 +0900

    wip

--- stock/kabutan/data.py
+++ stock/kabutan/data.py
@@ -38,7 +38,7 @@ def get_stock_data(code: str, base_url: str = "https://kabutan.jp/stock/kabuka?c
             volume = convert_to_number(tdata[6].text)
             daily_data.append([date, start, high, low, end, volume])
 
-    for row in daily_data:
-        print(row)
+    # for row in daily_data:
+    #     print(row)
 
     return daily_data
