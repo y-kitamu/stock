3f1564e869cc186726fd310eacc8f0832fb48dfb
kitamura <ymyk6602@gmail.com>
2024-07-18 22:44:44 +0900

    wip

--- stock/dl/__init__.py
+++ stock/dl/__init__.py
@@ -5,10 +5,13 @@ Create Date : 2024-07-13 22:07:43
 Copyright (c) 2019- Yusuke Kitamura <ymyk6602@gmail.com>
 """
 
-import gnn
+try:
+    import gnn
 
-from .dataloader import DataLoader
-from .network import Network
+    from .dataloader import DataLoader
+    from .network import Network
 
-gnn.dataloader.dataloader_list["stock_dataloader"] = DataLoader
-gnn.layers.network_list["stock_network"] = Network
+    gnn.dataloader.dataloader_list["stock_dataloader"] = DataLoader
+    gnn.layers.network_list["stock_network"] = Network
+except:
+    print("Failed to load gnn module.")
